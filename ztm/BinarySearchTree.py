a28e4edfcba4537b4e474c3cb93e49bb9795a723
Kerinjo <konmarciniak@gmail.com>
2023-02-27 17:48:09 +0100

    Binary Search Tree with insert method

---
+++ ztm/BinarySearchTree.py
@@ -0,0 +1,29 @@
+from Node import Node
+
+class BST():
+    def __init__(self):
+        self.root = None
+        self.number_of_nodes = 0
+    
+    def insert(self, data):
+        new_node = Node(data)
+        if self.root == None:
+            self.root = new_node
+            self.number_of_nodes += 1
+            return
+        else:
+            current_node = self.root
+            while(current_node.left != new_node and
+                  current_node.right != new_node):
+                if new_node.data > current_node.data:
+                    if current_node.right == None:
+                        current_node.right = new_node
+                    else:
+                        current_node = current_node.right
+                if new_node.data < current_node.data:
+                    if current_node.left == None:
+                        current_node.left = new_node
+                    else:
+                        current_node = current_node.left
+            self.number_of_nodes += 1
+            return
